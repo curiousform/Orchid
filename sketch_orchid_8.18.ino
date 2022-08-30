dd9aa7d068818f7ed3f43a77d9bc8942917afb1f
curiousform <111610296+curiousform@users.noreply.github.com>
2022-08-29 22:32:58 -0400

    Monday Night working midi messages 1032pm

--- sketch_orchid_8.18.ino
+++ sketch_orchid_8.18.ino
@@ -4,13 +4,9 @@
 MIDI_CREATE_DEFAULT_INSTANCE();
 
 // bounce time in ms
-int bounceInterval = 200;
+int bounceInterval = 200;                                                     //this is the time delay on each sensor trigger
 // debouncer for each sensor pin used
 Bounce bouncer[30] = Bounce();
-
-//  number of ms delay for on and off switch
-int delTime = 50;
-
 
 // Pin of first sensor
 int pinStart = 2;
@@ -52,11 +48,21 @@ int audStart = 40;
 //  Counter var for how many people are sitting down
 int counter = 0;
 int lastCounter = 0;
+//AUDIO DROP DOWN TIMER INTERVAL
+int counterInterval = 2000;                                                    //this is for time it takes for audio to drop back down after triggering
 
 //  COUNTER THRESHOLDS FOR ADDING MORE AUDIO LAYERS
-int layer[] = {2, 4, 8, 12, 18, 24}; //24 is for party mode
-int layerCount = sizeof(layer) / sizeof(int);
-int trend;
+int layer[] = {2, 4, 8, 12, 18, 24}; // 24 is for party mode
+const int layerCount = sizeof(layer) / sizeof(int);
+int trend = 0;
+
+long layerTimer[layerCount];
+int deferredAudMidiNote[layerCount];
+
+long lastRandomize = millis();
+long randomizeInterval = 30 * 60 * 1000;                                    //this is the time it takes for the tracks to be shuffled
+int randomOffset[] = {0, 1, 2, 3, 4};
+bool multiVal = false;
 
 void setup()
 {
@@ -85,24 +91,22 @@ void loop()
     if (bouncer[i].changed())
     {
       buttonState[i] = bouncer[i].read();
-
       // Pin pairing logic (Quantum Enganglement)
-      if ((i < multiCount))
-      {
-        if (bouncer[i].changed() || bouncer[i + multiCount].changed())
+      if (i < multiCount) {
+        bouncer[i + multiCount].update();
+        buttonState[i + multiCount] = bouncer[i + multiCount].read();
+        if ((buttonState[i + multiCount] == HIGH) && (buttonState[i] == HIGH))
         {
-          if ((buttonState[i] == buttonState[i + multiCount]) && (buttonState[i] == HIGH))
-          {
-            // sendNoteX(note, velocity, midiChannel)
-            MIDI.sendNoteOn(i + litStart, 120, multiChan);
-          }
-          else
-          {
-            MIDI.sendNoteOff(i + litStart, 0, multiChan);
-          }
+          // sendNoteX(note, velocity, midiChannel)
+          MIDI.sendNoteOn(i + litStart, 120, multiChan);
+          multiVal = true;
+        }
+        else if (multiVal)
+        {
+          MIDI.sendNoteOff(i + litStart, 0, multiChan);
+          multiVal = false;
         }
       }
-
 
       // If somebody sits down on any sensor thats non easter egg
       // Then send lighting midi note
@@ -133,8 +137,8 @@ void loop()
     }
   }
 
-
   // Audio layer count trigger
+  // Implement nonblocking delay on counter decrement
 
   if (counter != lastCounter)
   {
@@ -142,20 +146,56 @@ void loop()
 
     for (int i = 0; i < layerCount; i++)
     {
-      if (trend > 0 && counter == layer[i])
+      if (trend > 0 && counter == layer[i] && i < layerCount - 1)
       {
-        //item is increasing and equal to threshold
-        MIDI.sendNoteOn(i + audStart, 120, audChan);
+        // item is increasing and equal to threshold
+        MIDI.sendNoteOn( audStart + randomOffset[i], 120, audChan);
+        MIDI.sendNoteOff( audStart + randomOffset[i], 0, audChan);
+      }
+      else if (trend > 0 && counter == layer[i] && i == layerCount - 1)
+      {
+        MIDI.sendNoteOn(i +  audStart, 120, audChan);
         MIDI.sendNoteOff(i + audStart, 0, audChan);
       }
+
       else if (trend < 0 && counter == (layer[i] - 1))
       {
         // item is decreasing and dropping threshold level
-        MIDI.sendNoteOn(i + audStart + layerCount, 120, audChan);
-        MIDI.sendNoteOff(i + audStart + layerCount, 0, audChan);
+        deferredAudMidiNote[i] = (randomOffset[i] + audStart + layerCount);
+        layerTimer[i] = millis();
       }
-
     }
   }
+
+  for (int i = 0; i < layerCount - 1; i++)
+  {
+    if (millis() - layerTimer[i] >= counterInterval && deferredAudMidiNote[i] != 0)
+    {
+      MIDI.sendNoteOn(deferredAudMidiNote[i], 120, audChan);
+      delay(100);
+      MIDI.sendNoteOff(deferredAudMidiNote[i], 0, audChan);
+      deferredAudMidiNote[i] = 0;
+    }
+  }
+
+  if (millis() - lastRandomize >= randomizeInterval)
+  {
+    randomSeed(analogRead(0));
+    lastRandomize = millis();
+
+    if (trend > 0 && counter < layer[layerCount - 1] || trend == 0)
+    {
+      randomOffset[layerCount] = random(0, layerCount - 1);
+
+      for (int a = 0; a < layerCount - 1; a++)
+      {
+        int r = random(a, layerCount - 1); // dont remember syntax just now, random from a to 8 included.
+        int temp = randomOffset[a];
+        randomOffset[a] = randomOffset[r];
+        randomOffset[r] = temp;
+      }
+    }
+  }
+
   lastCounter = counter;
 }
